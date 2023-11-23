60c79d3e237ebee9fcfdea2e76d4b932c4db6740
RazanAlkhaluqy <razan08642@gmail.com>
2023-11-23 20:54:38 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -7,4 +7,28 @@ document.addEventListener('DOMContentLoaded', function () {
     mobileMenuIcon.addEventListener('click', function () {
         navList.classList.toggle('show');
     });
+
+    const customizeButton = document.getElementById('customize-button');
+    const themeStylesheet1 = document.getElementById('theme-stylesheet');
+    const themeStylesheet2 = document.querySelector('link[href="theme2.css"]');
+    let isDarkMode = false;
+
+    customizeButton.addEventListener('click', function () {
+        // Toggle between themes
+        if (themeStylesheet1.disabled) {
+            themeStylesheet1.removeAttribute('disabled');
+            themeStylesheet2.setAttribute('disabled', 'true');
+            document.body.classList.remove('theme2');
+            document.body.classList.add('theme1');
+        } else {
+            themeStylesheet1.setAttribute('disabled', 'true');
+            themeStylesheet2.removeAttribute('disabled');
+            document.body.classList.remove('theme1');
+            document.body.classList.add('theme2');
+        }
+        // Toggle Dark Mode / Light Mode text
+        isDarkMode = !isDarkMode;
+        customizeButton.textContent = isDarkMode ? 'Light Mode' : 'Dark Mode';
+    });
 });
+
