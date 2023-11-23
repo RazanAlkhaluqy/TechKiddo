3589d2276027d01eead5582f3191117561c2db29
RazanAlkhaluqy <razan08642@gmail.com>
2023-11-23 23:29:35 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,34 +1,43 @@
 
 // Toggle the mobile menu on/off
-document.addEventListener('DOMContentLoaded', function () {
-    const mobileMenuIcon = document.querySelector('.mobile-menu-icon');
-    const navList = document.querySelector('.nav-list');
 
-    mobileMenuIcon.addEventListener('click', function () {
-        navList.classList.toggle('show');
-    });
-
-    const customizeButton = document.getElementById('customize-button');
-    const themeStylesheet1 = document.getElementById('theme-stylesheet');
-    const themeStylesheet2 = document.querySelector('link[href="theme2.css"]');
-    let isDarkMode = false;
-
-    customizeButton.addEventListener('click', function () {
-        // Toggle between themes
-        if (themeStylesheet1.disabled) {
-            themeStylesheet1.removeAttribute('disabled');
-            themeStylesheet2.setAttribute('disabled', 'true');
-            document.body.classList.remove('theme2');
-            document.body.classList.add('theme1');
-        } else {
-            themeStylesheet1.setAttribute('disabled', 'true');
-            themeStylesheet2.removeAttribute('disabled');
-            document.body.classList.remove('theme1');
-            document.body.classList.add('theme2');
+    document.addEventListener('DOMContentLoaded', function () {
+        const mobileMenuIcon = document.querySelector('.mobile-menu-icon');
+        const navList = document.querySelector('.nav-list');
+    
+        mobileMenuIcon.addEventListener('click', function () {
+            navList.classList.toggle('show');
+        });
+    
+        const customizeButton = document.getElementById('customize-button');
+        let isDarkMode = localStorage.getItem('isDarkMode') === 'true';
+    
+        // Function to toggle theme
+        function toggleTheme() {
+            const themeStylesheet1 = document.getElementById('theme-stylesheet');
+            const themeStylesheet2 = document.querySelector('link[href="theme2.css"]');
+    
+            if (isDarkMode) {
+                themeStylesheet1.removeAttribute('disabled');
+                themeStylesheet2.setAttribute('disabled', 'true');
+            } else {
+                themeStylesheet1.setAttribute('disabled', 'true');
+                themeStylesheet2.removeAttribute('disabled');
+            }
+    
+            // Toggle Dark Mode / Light Mode text
+            isDarkMode = !isDarkMode;
+    
+            // Update the button text on all pages
+            customizeButton.textContent = isDarkMode ? 'Dark Mode' : 'Light Mode';
+    
+            // Save the user's preference to localStorage
+            localStorage.setItem('isDarkMode', isDarkMode.toString());
         }
-        // Toggle Dark Mode / Light Mode text
-        isDarkMode = !isDarkMode;
-        customizeButton.textContent = isDarkMode ? 'Light Mode' : 'Dark Mode';
+    
+        // Set initial theme based on user's preference
+        toggleTheme();
+    
+        customizeButton.addEventListener('click', toggleTheme);
     });
-});
-
+    
